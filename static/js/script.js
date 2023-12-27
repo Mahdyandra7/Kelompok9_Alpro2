61cbeec10580731b2243a20fc1ae381948c6eb4b
Mahdyandra7 <mahdyandra7903@gmail.com>
2023-12-27 16:28:25 +0700

    bismillah bisa

--- static/js/script.js
+++ static/js/script.js
@@ -5,13 +5,13 @@ selectTag = document.querySelectorAll("select"),
 icons = document.querySelectorAll(".row i");
 const translateBtn = document.querySelector("button");
 
-selectTag.forEach((tag, id) => {
-    for (let country_code in countries) {
-        let selected = id == 0 ? country_code == "en-GB" ? "selected" : "" : country_code == "hi-IN" ? "selected" : "";
-        let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
-        tag.insertAdjacentHTML("beforeend", option);
-    }
-});
+// selectTag.forEach((tag, id) => {
+//     for (let country_code in countries) {
+//         let selected = id == 0 ? country_code == "en-GB" ? "selected" : "" : country_code == "hi-IN" ? "selected" : "";
+//         let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
+//         tag.insertAdjacentHTML("beforeend", option);
+//     }
+// });
 
 exchageIcon.addEventListener("click", () => {
     let tempText = fromText.value,
